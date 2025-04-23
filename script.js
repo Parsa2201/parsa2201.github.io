0354428c61bff1700794fe7cf32de0c5f9a95e99
parsa2201 <sa.parsa20@gmail.com>
2025-04-24 01:46:41 +0330

    added colors and shadows

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+
+function changeBackgroundOnScroll() {
+    const scrollPosition = window.scrollY;
+    const documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercentage = (scrollPosition / documentHeight) * 100;
+    
+    
+    const maxHue = 360;
+    const offset = 100;
+    
+    const hue = (Math.sin(scrollPercentage / 200 * Math.PI * 2) * 180 + offset) % maxHue;
+    
+    document.body.style.backgroundColor = `hsl(${hue}, 20%, 95%)`;
+  }
+  
+
+  window.addEventListener('scroll', changeBackgroundOnScroll);
